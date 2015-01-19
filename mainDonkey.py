785d4f951c70eb6a4821fb0a9ebf6ea2637ab06b
gkamradt <gkamradt@gmail.com>
2015-01-18 20:22:08 -0500

    Update MontyHall

--- mainDonkey.py
+++ mainDonkey.py
@@ -9,27 +9,27 @@ def percent(val, digits):
     val *= 10 ** (digits + 2)
     return '{1:.{0}f}%'.format(digits, floor(val) / 10 ** digits)
 
-def donkey(rounds, x, p):
+def donkey(rounds, switching, printResult): # number of rounds,
     wincount = 0.0
     i = 0
     while i < rounds:
         win = False
-        take = random.randint(0,2)
         holder = 0
         choice = ["Don","Don","Car"]
         random.shuffle(choice)
-        select = random.randint(0,2)
-        if x: #if you select true to switch
-            holder = select
+        take = random.randint(0,2) #initially setting which value you'll take first
+        select = random.randint(0,2) #making your original selection
+        if switching: #if you select true to switch
+            holder = select #make sure when you select a new value, its not your original one
             while take == select or choice[take] == "Car": #youre taking one away thats not your selection or the car
                 take = random.randint(0,2)
-            while select == take or select == holder:
+            while select == take or select == holder: #changing your selection
                 select = random.randint(0,2)
             if choice[select] == "Car": #now that you changed your selection, see if you're right
                 wincount += 1
                 i += 1
                 win = True
-                if p:
+                if printResult: #if you want to see all the rounds played out
                     print i, choice, "first", holder, "away", take, choice[take], "changed", select, win
                 continue
             else:
@@ -42,14 +42,13 @@ def donkey(rounds, x, p):
                 continue
             else:
                 i += 1
-        if p:
+        if printResult:
             print i, choice, "first", holder, "away", take, choice[take], "changed", select, win
 
     f = lambda x: "Switch" if x else "No Switch"
 
-    print f(x), int(wincount), "Out Of", i, "or", percent(wincount/i,1)
+    print f(switching), int(wincount), "Out Of", i, "or", percent(wincount/i,1)
 
-donkey(10000, True, False)
-donkey(10000, False, False)
-
-print time.time()-start 
+donkey(100000, True, False)
+donkey(100000, False, False)
+print time.time()-cycle1
